5d9be55cbe39222133e3c131f16f3bd1c6d8806b
Sardoroff <eshpolatovsardor18@gmail.com>
2025-02-21 20:38:15 +0500

    sardor

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -420,7 +420,7 @@ const _5w4pSTPsND = (function(nitro) {
 
 const rootDir = "C:/Users/Administrator/Desktop/PRODUCT REAL/gilam";
 
-const appHead = {"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"name":"apple-mobile-web-app-status-bar-style","content":"black-translucent"}],"link":[],"style":[],"script":[],"noscript":[],"viewport":"width=device-width,initial-scale=1"};
+const appHead = {"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"name":"apple-mobile-web-app-status-bar-style","content":"black-translucent"},{"name":"description","content":"Home Clean Uz – professional uy va mebel yuvish xizmati. Uy, gilam, mebel va avtomobil salonlarini tozalash bo‘yicha sifatli xizmatlar!"},{"name":"keywords","content":"uy tozalash, gilam yuvish, mebel yuvish, tozalash xizmati, professional tozalash, Home Clean Uz"},{"name":"robots","content":"index, follow"},{"property":"og:title","content":"Home Clean Uz - Professional Uy va Mebel Yuvish Xizmati"},{"property":"og:description","content":"Professional gilam, mebel va uy tozalash xizmatlari. Sifatli va ekologik toza yuvish xizmatlari!"},{"property":"og:type","content":"website"},{"name":"twitter:title","content":"Home Clean Uz - Professional Uy va Mebel Yuvish Xizmati"},{"name":"twitter:description","content":"Uy, gilam va mebel tozalash xizmatlari. Tajribali mutaxassislar va ekologik xavfsiz yuvish vositalari!"}],"link":[],"style":[],"script":[],"noscript":[],"viewport":"width=device-width,initial-scale=1"};
 
 const appRootTag = "div";
 
